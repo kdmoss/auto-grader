f8ad7fcd7df1c52e9e65a002c8525fe39a87e934
Kaid Mossburgh <kdmossbu@users.campus>
2020-01-31 13:52:53 -0600

    Begin work on pagination

--- grader.py
+++ grader.py
@@ -3,7 +3,13 @@ import requests
 token = "1726~bpIkiAOKiYLhv0KOfX7ScaX0ggeK0qEliRh34tqIjnPintrBLLX7nfe6cXEx1uF7"
 endpoint = "https://k-state.instructure.com/api/v1/courses/89156/"
 
-request_data = {"access_token": token, "per_page": 400}
+request_data = {
+    "access_token": token,
+    "per_page": 100,
+    "enrollment_type": "student"
+}
+
+# Deal with pagination by reviewing the "link" response header
 users_response = requests.get("{}/users".format(endpoint), data=request_data)
 
 print(users_response.text)
