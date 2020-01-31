9d57ca12837ae45e7cc4323e18b4e1783a08ea8f
Kaid Mossburgh <kdmossbu@users.campus>
2020-01-31 13:42:06 -0600

    Change single quotes to double quotes

--- grader.py
+++ grader.py
@@ -3,7 +3,7 @@ import requests
 token = "1726~bpIkiAOKiYLhv0KOfX7ScaX0ggeK0qEliRh34tqIjnPintrBLLX7nfe6cXEx1uF7"
 endpoint = "https://k-state.instructure.com/api/v1/courses/89156/"
 
-request_data = {'access_token': token, 'per_page': 400}
+request_data = {"access_token": token, "per_page": 400}
 users_response = requests.get("{}/users".format(endpoint), data=request_data)
 
 print(users_response.text)
