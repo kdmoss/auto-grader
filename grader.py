a3d386b5e51675cf39886bf7a30b4fe3570506b4
Kaid Mossburgh <kdmossbu@users.campus>
2020-01-31 13:39:52 -0600

    Consolidate Users GET request

--- grader.py
+++ grader.py
@@ -3,8 +3,7 @@ import requests
 token = "1726~bpIkiAOKiYLhv0KOfX7ScaX0ggeK0qEliRh34tqIjnPintrBLLX7nfe6cXEx1uF7"
 endpoint = "https://k-state.instructure.com/api/v1/courses/89156/"
 
-users_response = requests.get(
-    "{0}/users?access_token={1}&per_page=1000".format(endpoint, token))
+request_data = {'access_token': token, 'per_page': 400}
+users_response = requests.get("{}/users".format(endpoint), data=request_data)
 
-
-print(users_response.status_code)
+print(users_response.text)
